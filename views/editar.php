63182f0126a62aa7a4e1fe1205ec88b8f8e18e51
Tiao-do-gas <joaodonazan@aluno.senai.br>
2024-12-02 12:51:01 -0300

    alterações BIEL

--- views/editar.php
+++ views/editar.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Editar Informações - SP Medical Group</title>
-    <link rel="stylesheet" href="/Projeto-Final/views/editar.css">
+    <link rel="stylesheet" href="/Projeto-Final-Clinica/views/editar.css">
    
 
 </head>
@@ -19,32 +19,32 @@
     <input type="hidden" name="id" value="<?= $user['id']; ?>">
 
             <div class="input-group">
-                <img src="/PROJETO-FINAL-CLINICA/views/sem_login/imagens/image6.png" alt="Ícone de usuário" class="icon user-icon">
+                <img src="/Projeto-Final-Clinica/views/imagens/image6.png" alt="Ícone de usuário" class="icon user-icon">
                 <input type="text" name="nome" value="<?= $user['nome']; ?>" placeholder="Nome completo" required>
             </div>
 
             <div class="input-group">
-                <img src="../../PROJETO-FINAL-CLINICA/views/sem_login/imagens/image6.png" alt="Ícone de usuário" class="icon user-icon">
+                <img src="/PROJETO-FINAL-CLINICA/views/imagens/image6.png" alt="Ícone de usuário" class="icon user-icon">
                 <input type="email" name="email" value="<?= $user['email']; ?>" placeholder="Email" required>
             </div>
 
             <div class="input-group">
-                <img src="imagens/image7.png" alt="Ícone de chave" class="icon key-icon">
+                <img src="/PROJETO-FINAL-CLINICA/views/imagens/image6.png" alt="Ícone de chave" class="icon key-icon">
                 <input type="password" name="senha" placeholder="Nova senha" required>
             </div>
 
             <div class="input-group">
-                <img src="imagens/image 7.png" alt="Ícone de chave" class="icon key-icon">
+                <img src="/PROJETO-FINAL-CLINICA/views/imagens/image6.png" alt="Ícone de chave" class="icon key-icon">
                 <input type="password" name="confirmar_senha" placeholder="Confirmar nova senha" required>
             </div>
 
             <div class="input-group">
-                <img src="imagens/pin-7697709_1280.webp" alt="Ícone de endereço" class="icon user-icon">
+                <img src="/PROJETO-FINAL-CLINICA/views/imagens/image6.png" alt="Ícone de endereço" class="icon user-icon">
                 <input type="text" name="endereco" value="<?= $user['endereco']; ?>" placeholder="Endereço" required>
             </div>
 
             <div class="input-group">
-                <img src="imagens/5782919.png" alt="Ícone de CPF" class="icon user-icon">
+                <img src="/PROJETO-FINAL-CLINICA/views/imagens/image6.png" alt="Ícone de CPF" class="icon user-icon">
                 <input type="text" name="cpf" value="<?= $user['cpf']; ?>" placeholder="CPF" required>
             </div>
 
